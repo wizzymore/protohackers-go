c774418562383cfb424b0129ad8bf971561e4197
Cristian Bilu <cristian@eptic.ro>
2025-05-03 23:50:49 +0300

    Another way of the same thing

--- main.go
+++ main.go
@@ -49,13 +49,24 @@ func main() {
 }
 
 func step_zero(conn net.Conn) {
+	log := log.With().Str("remote_addr", conn.RemoteAddr().String()).Logger()
 	defer func() {
-		log.Info().Str("remote_addr", conn.RemoteAddr().String()).Msgf("Closing connection from %s", conn.RemoteAddr())
+		log.Info().Str("remote_addr", conn.RemoteAddr().String()).Msg("Closing connection")
 		conn.Close()
 	}()
 
-	if _, err := io.Copy(conn, conn); err != nil {
-		log.Error().Err(err).Msg("Error copying data")
-		return
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				log.Info().Msg("Client closed the connection")
+				return
+			}
+			log.Err(err).Msg("Client read error")
+			return
+		}
+
+		conn.Write(buf[:n])
 	}
 }
