c7b18e598be07b3d513476bd3b9606af957f9f0c
Cristian Bilu <cristian@eptic.ro>
2026-05-17 04:28:06 +0300

    Improve readability of mob test

--- mob/mob_test.go
+++ mob/mob_test.go
@@ -4,17 +4,18 @@ import (
 	"bufio"
 	"net"
 	"testing"
-	"time"
 
 	"github.com/wizzymore/tcp-go/mob"
 )
 
 func TestServer(t *testing.T) {
-	bogusServer, err := net.Listen("tcp", ":16963")
+	// Initialize protohackers mock server
+	server, err := net.Listen("tcp", ":16963")
 	if err != nil {
 		t.Fatal("Could not create bogus server", err)
 	}
 
+	// Initialize our proxy server
 	proxyServer, err := mob.NewMobServer("127.0.0.1:16963")
 	if err != nil {
 		t.Fatal("Could not create mob server", err)
@@ -23,25 +24,29 @@ func TestServer(t *testing.T) {
 	go proxyServer.Start()
 	defer proxyServer.Stop()
 
-	conn, err := net.Dial("tcp", ":8081")
+	// Connect client to proxy
+	client, err := net.Dial("tcp", ":8081")
 	if err != nil {
 		t.Fatal("Could not connect to mob server", err)
 	}
 
-	t.Log("Waiting for connection on bogus server")
-	bConn, err := bogusServer.Accept()
+	// Accept the connection from the proxy on behalf of the client
+	server_conn, err := server.Accept()
 	if err != nil {
 		t.Fatal("Could not accept proxy connection on bogus", err)
 	}
-	t.Log("Received connection on bogus server")
-	bWriter := bufio.NewWriter(bConn)
-	welcomeMessage := "Welcome to budgetchat! What shall I call you?\n"
-	bWriter.WriteString(welcomeMessage)
-	bWriter.Flush()
 
-	cReader := bufio.NewReader(conn)
-	conn.SetReadDeadline(time.Now().Add(time.Millisecond * 200))
-	result, err := cReader.ReadString('\n')
+	// Send welcome message, the proxy should pass this to our client
+	welcomeMessage := "Welcome to budgetchat! What shall I call you?\n"
+
+	server_reader := bufio.NewReader(server_conn)
+	server_writer := bufio.NewWriter(server_conn)
+	server_writer.WriteString(welcomeMessage)
+	server_writer.Flush()
+
+	client_reader := bufio.NewReader(client)
+	client_writer := bufio.NewWriter(client)
+	result, err := client_reader.ReadString('\n')
 	if err != nil {
 		t.Fatal("Could not read from mob server", err)
 	}
@@ -50,12 +55,11 @@ func TestServer(t *testing.T) {
 		t.Fatal("Did not received welcome message, got: ", result)
 	}
 
-	cWriter := bufio.NewWriter(conn)
-	cWriter.WriteString("Hi alice, please send payment to 7F1u3wSD5RbOHQmupo9nx4TnhQ\n")
-	cWriter.Flush()
+	// Test that all wallet ids gets replaced with the bogus value 7YWHMfk9JZe0LM0g1ZauHuiSxhI
+	client_writer.WriteString("Hi alice, please send payment to 7F1u3wSD5RbOHQmupo9nx4TnhQ\n")
+	client_writer.Flush()
 
-	bReader := bufio.NewReader(bConn)
-	result, err = bReader.ReadString('\n')
+	result, err = server_reader.ReadString('\n')
 	if err != nil {
 		t.Fatal("Could not read message on bogus", err)
 	}
