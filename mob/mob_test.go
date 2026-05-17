7c3cbe11cfc3749789bb236e9724df3c1894e10e
Cristian Bilu <cristian@eptic.ro>
2026-05-17 05:02:09 +0300

    Improve mob tests

--- mob/mob_test.go
+++ mob/mob_test.go
@@ -2,16 +2,22 @@ package mob_test
 
 import (
 	"bufio"
+	"bytes"
 	"net"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/wizzymore/tcp-go/mob"
 )
 
 func TestServer(t *testing.T) {
+	log.Logger = zerolog.New(&bytes.Buffer{})
+
 	// Initialize protohackers mock server
 	server, err := net.Listen("tcp", ":16963")
 	require.NoError(t, err, "Could not create bogus server")
@@ -23,36 +29,57 @@ func TestServer(t *testing.T) {
 	go proxyServer.Start()
 	defer proxyServer.Stop()
 
-	// Connect client to proxy
-	client, err := net.Dial("tcp", ":8081")
+	// Connect client_con to proxy
+	client_con, err := net.Dial("tcp", ":8081")
 	require.NoError(t, err, "Could not connect to mob server")
 
 	// Accept the connection from the proxy on behalf of the client
 	server_conn, err := server.Accept()
 	require.NoError(t, err, "Could not accept proxy connection on bogus")
 
-	// Send welcome message, the proxy should pass this to our client
-	welcomeMessage := "Welcome to budgetchat! What shall I call you?\n"
+	t.Run("forwards server messages to client", func(t *testing.T) {
+		// Send welcome message, the proxy should pass this to our client
+		welcomeMessage := "Welcome to budgetchat! What shall I call you?\n"
 
-	server_reader := bufio.NewReader(server_conn)
-	server_writer := bufio.NewWriter(server_conn)
-	server_writer.WriteString(welcomeMessage)
-	server_writer.Flush()
+		server_writer := bufio.NewWriter(server_conn)
+		server_writer.WriteString(welcomeMessage)
+		server_writer.Flush()
 
-	client_reader := bufio.NewReader(client)
-	client_writer := bufio.NewWriter(client)
-	result, err := client_reader.ReadString('\n')
-	require.NoError(t, err, "Could not read from server")
+		client_reader := bufio.NewReader(client_con)
+		result, err := client_reader.ReadString('\n')
+		require.NoError(t, err, "Could not read from server")
 
-	assert.Equal(t, result, welcomeMessage, "Did not receive welcome message")
+		assert.Equal(t, result, welcomeMessage, "Did not receive welcome message")
+	})
 
-	// Test that all wallet ids gets replaced with the bogus value 7YWHMfk9JZe0LM0g1ZauHuiSxhI
-	client_writer.WriteString("Hi alice, please send payment to 7F1u3wSD5RbOHQmupo9nx4TnhQ\n")
-	client_writer.Flush()
+	t.Run("receives messages from client", func(t *testing.T) {
+		// Test that all wallet ids gets replaced with the bogus value 7YWHMfk9JZe0LM0g1ZauHuiSxhI
+		client_con.SetWriteDeadline(time.Now().Add(time.Millisecond * 200))
+		client_writer := bufio.NewWriter(client_con)
+		client_writer.WriteString("Alice\n")
+		client_writer.Flush()
 
-	result, err = server_reader.ReadString('\n')
-	require.NoError(t, err, "Could not read message on bogus")
+		server_conn.SetReadDeadline(time.Now().Add(time.Millisecond * 200))
+		server_reader := bufio.NewReader(server_conn)
+		result, err := server_reader.ReadString('\n')
+		require.NoError(t, err, "Could not read message on server")
 
-	assert.Equal(t, result, "Hi alice, please send payment to 7YWHMfk9JZe0LM0g1ZauHuiSxhI\n", "Did not replace token with BOGUS token")
-	assert.True(t, !strings.Contains(result, "7F1u3wSD5RbOHQmupo9nx4TnhQ"), "Should not contain original wallet id")
+		assert.Equal(t, result, "Alice\n")
+	})
+
+	t.Run("modifies wallet id with bogus id", func(t *testing.T) {
+		// Test that all wallet ids gets replaced with the bogus value 7YWHMfk9JZe0LM0g1ZauHuiSxhI
+		client_con.SetWriteDeadline(time.Now().Add(time.Millisecond * 200))
+		client_writer := bufio.NewWriter(client_con)
+		client_writer.WriteString("Hi alice, please send payment to 7F1u3wSD5RbOHQmupo9nx4TnhQ\n")
+		client_writer.Flush()
+
+		server_conn.SetReadDeadline(time.Now().Add(time.Millisecond * 200))
+		server_reader := bufio.NewReader(server_conn)
+		result, err := server_reader.ReadString('\n')
+		require.NoError(t, err, "Could not read message on bogus")
+
+		assert.Equal(t, result, "Hi alice, please send payment to 7YWHMfk9JZe0LM0g1ZauHuiSxhI\n", "Did not replace token with BOGUS token")
+		assert.True(t, !strings.Contains(result, "7F1u3wSD5RbOHQmupo9nx4TnhQ"), "Should not contain original wallet id")
+	})
 }
